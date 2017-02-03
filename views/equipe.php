68d95c5f7af47eaa497fb934bc4f761ecf3ef4f6
JOKERPULSATION <hamelinpaul@hotmail.fr>
2017-02-03 14:16:37 +0100

    equipe

--- views/equipe.php
+++ views/equipe.php
@@ -14,20 +14,146 @@
 			<h3>La classe</h3>	
 			<div class="text-center">
 				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
-					<img src="images/services/1.jpg" alt="" >
-					<h4>John Doe</h4>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+					<img src="./vendor/img/Pierre.jpg" alt="" >
+					<h4>Pierre</h4>
+					<p>Toshiba Warrior</p>
 				</div>
 				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
-					<img src="images/services/2.jpg" alt="" >
-					<h4>John Doe</h4>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+					<img src="./vendor/img/elisa.jpg" alt="" >
+					<h4>Elisa</h4>
+					<p>WordPress Warrior</p>
 				</div>
 				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
-					<img src="images/services/3.jpg" alt="" >
-					<h4>John Doe</h4>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+					<img src="./vendor/img/paul.jpg" alt="" >
+					<h4>Paul</h4>
+					<p>Hp Gaming Warrior</p>
 				</div>
 			</div>
 		</div>
 	</div>
+
+		<div class="our-team">
+			<div class="container">
+			
+			<div class="text-center">
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+					<img src="./vendor/img/matheus.jpg" alt="" >
+					<h4>Matheus</h4>
+					<p>Debian Super Master</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+					<img src="./vendor/img/julien.jpg" alt="" >
+					<h4>Julien</h4>
+					<p>Dofus Cancer</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+					<img src="./vendor/img/jean-francois.jpg" alt="" >
+					<h4>Jefweeey</h4>
+					<p>Jeff Samaaaaaaaaaaaaaaaaaa</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+			<div class="our-team">	
+			<div class="container">
+			<div class="text-center">
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+					<img src="./vendor/img/megane.jpg" alt="" >
+					<h4>Megane</h4>
+					<p>Laracroft  </p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+					<img src="./vendor/img/bastien.jpg" alt="" >
+					<h4>Bastien</h4>
+					<p>Petite Biche</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+					<img src="./vendor/img/marvin.jpg" alt="" >
+					<h4>Marvin</h4>
+					<p>Jean Dev</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+				<div class="our-team">	
+			<div class="container">
+			<div class="text-center">
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+					<img src="./vendor/img/florent.jpg" alt="" >
+					<h4>Florent</h4>
+					<p>Florence aprés 21h</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+					<img src="./vendor/img/matt.jpg" alt="" >
+					<h4>Matt</h4>
+					<p>Jean Black Mantis</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+					<img src="./vendor/img/pl.jpg" alt="" >
+					<h4>Pl</h4>
+					<p>Polak Nasdrovia</p>
+				</div>
+			</div>
+		</div>
+	</div>
+					<div class="our-team">	
+			<div class="container">
+			<div class="text-center">
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+					<img src="./vendor/img/christopher.jpg" alt="" >
+					<h4>Christo</h4>
+					<p>hasagi au sel de mer </p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+					<img src="./vendor/img/alexis.jpg" alt="" >
+					<h4>Alexis</h4>
+					<p>Calvitie précoce</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+					<img src="./vendor/img/simon.jpg" alt="" >
+					<h4>Simon</h4>
+					<p> Jean Simux</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+					<div class="our-team">	
+			<div class="container">
+			<div class="text-center">
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+					<img src="./vendor/img/kenny.jpg" alt="" >
+					<h4>Kenny</h4>
+					<p>Il ont tués Kenny</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+					<img src="./vendor/img/matthieu.jpg" alt="" >
+					<h4>Matthieu</h4>
+					<p>fils de sauron alias bureautique salée</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+					<img src="./vendor/img/yvan.jpg" alt="" >
+					<h4>Yvan</h4>
+					<p>Yvan du sel</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+					<div class="our-team">	
+			<div class="container">
+			<div class="text-center">
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+					<img src="./vendor/img/guillaume.jpg" alt="" >
+					<h4>Guillaume</h4>
+					<p>Gigs du 72</p>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
