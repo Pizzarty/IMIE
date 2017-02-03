f002c5008ce484d1251829704d6211ca04dcbc13
Pierre Banville <Pierre Banville>
2017-02-03 10:30:15 +0100

    Wesh

--- views/home.php
+++ views/home.php
@@ -1,5 +1,5 @@
 <section id="main-slider" class="no-margin">
-        <div class="carousel slide">      
+        <div class="carousel slide">
             <div class="carousel-inner">
                 <div class="item active" style="background-image: url(./vendor/img/banderole1.jpg)">
                     <div class="container">
@@ -7,14 +7,11 @@
 
 
                         <div class="row centered slide-margin">
-                                           
                             <div class="col-sm-3  hidden-xs animation animated-item-4 ">
                                 <div class="slider-img">
                                     <img src="./vendor/img/logo-imie.png" class="img-responsive">
                                 </div>
                             </div>
-                    
-
                             <div class="col-lg-12 text-center">
                                 <div class="carousel-content">
                                     <h2 class="animation animated-item-1">Bienvenue sur le site des <span>IT Start</span></h2>
@@ -22,6 +19,7 @@
                                     <a class="btn-slide animation animated-item-3" href="#">Read More</a>
                                 </div>
                             </div>
+                        </div>
 
 <!--                             <div class="col-sm-6 hidden-xs animation animated-item-4">
                                 <div class="slider-img">
@@ -31,7 +29,7 @@
 
                         </div>
                     </div>
-                </div><!--/.item-->             
+                </div><!--/.item-->
             </div><!--/.carousel-inner-->
         </div><!--/.carousel-->
 </section><!--/#main-slider-->
@@ -48,14 +46,14 @@
             <div class="text-center">
                 <div class="col-md-6">
                     <div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" >
-                        <i class="fa fa-book"></i>  
+                        <i class="fa fa-book"></i>
                         <h2>Ecole</h2>
                         <p>Présentation de l'école</p>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" >
-                        <i class="fa fa-laptop"></i>    
+                        <i class="fa fa-laptop"></i>
                         <h2>It Start</h2>
                         <p>Présentation de la classe</p>
                     </div>
@@ -75,27 +73,27 @@
             <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
                 <img src="images/4.jpg" class="img-responsive"/>
                 <h3>Template built with Twitter Bootstrap</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat
                 libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
                 libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
                 libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
                 </p>
             </div>
-            
+
             <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
                 <img src="images/4.jpg" class="img-responsive"/>
                 <h3>Template built with Twitter Bootstrap</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat
                 libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
                 libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
                 libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
                 </p>
             </div>
-            
-            <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">             
+
+            <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
                 <img src="images/4.jpg" class="img-responsive"/>
                 <h3>Template built with Twitter Bootstrap</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat
                 libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
                 libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
                 libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
