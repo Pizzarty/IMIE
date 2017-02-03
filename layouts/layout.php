d23b6b2939050da19f3d91bfda0c43c6ddb00da4
Eliiisa <elisaklein66@gmail.com>
2017-02-03 11:47:22 +0100

    vghuog

--- layouts/layout.php
+++ layouts/layout.php
@@ -76,12 +76,12 @@
 	</footer>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="./vendor/js/jquery-2.1.1.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.prettyPhoto.js"></script>
-    <script src="js/jquery.isotope.min.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script src="js/functions.js"></script>
+    <script src="./vendor/js/bootstrap.min.js"></script>
+    <script src="./vendor/js/jquery.prettyPhoto.js"></script>
+    <script src="./vendor/js/jquery.isotope.min.js"></script>
+    <script src="./vendor/js/wow.min.js"></script>
+    <script src="./vendor/js/functions.js"></script>
   </body>
 </html>
