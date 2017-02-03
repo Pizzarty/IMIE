aabd54132b7337ee3b5a893282d93aa61b6afd50
Eliiisa <elisaklein66@gmail.com>
2017-02-03 10:27:39 +0100

    page photo gens classe

---
+++ views/equipe.php
@@ -0,0 +1,22 @@
+	<div class="our-team">
+		<div class="container">
+			<h3>La classe</h3>	
+			<div class="text-center">
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+					<img src="images/services/1.jpg" alt="" >
+					<h4>John Doe</h4>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+					<img src="images/services/2.jpg" alt="" >
+					<h4>John Doe</h4>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+				</div>
+				<div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+					<img src="images/services/3.jpg" alt="" >
+					<h4>John Doe</h4>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+				</div>
+			</div>
+		</div>
+	</div>
