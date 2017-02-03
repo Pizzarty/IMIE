9a5284d3a544d7a61216085182e49a28ff901bc0
JOKERPULSATION <hamelinpaul@hotmail.fr>
2017-02-03 15:53:00 +0100

    equipe-final

--- layouts/layout.php
+++ layouts/layout.php
@@ -45,11 +45,10 @@
 				<div class="social-icon">
 					<div class="col-md-4">
 						<ul class="social-network">
-							<li><a href="#" class="fb tool-tip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#" class="twitter tool-tip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-							<li><a href="#" class="gplus tool-tip" title="Google Plus"><i class="fa fa-google-plus"></i></a></li>
-							<li><a href="#" class="linkedin tool-tip" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-							<li><a href="#" class="ytube tool-tip" title="You Tube"><i class="fa fa-youtube-play"></i></a></li>
+							<li><a target="blank" href="https://www.facebook.com/IMIE.IT/?fref=ts" class="fb tool-tip" title="Facebook" ><i class="fa fa-facebook"></i></a></li>
+							<li><a target="blank" href="https://twitter.com/ecoleimie" class="twitter tool-tip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+							<li><a target="blank" href="https://www.linkedin.com/company/imie---institut-de-la-fili-re-num-rique?trk=company_logo" class="linkedin tool-tip" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+							<li><a target="blank" href="https://www.youtube.com/channel/UCzQ2cTutk2oFDDWA9IaCTbA" class="ytube tool-tip" title="You Tube"><i class="fa fa-youtube-play"></i></a></li>
 						</ul>
 					</div>
 				</div>
